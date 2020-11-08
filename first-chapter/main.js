bc855ab0de12c943f7377563fed0ee393ee8cbbc
tamaritamari <tamari@minacare.co.jp>
2020-11-09 01:43:50 +0900

    format

--- first-chapter/main.js
+++ first-chapter/main.js
@@ -1,4 +1,7 @@
 function statement(invoice, plays) {
+  function format(aNumber) {
+    return new Intl.NumberFormat("en-US", {style: 'currency', currency: "USD", minimumFractionDigits: 2}).format(aNumber);
+  }
   function volumeCreditsFor(aPerformance) {
     let result = 0;
     result += Math.max(aPerformance.audience - 30, 0)
@@ -35,7 +38,6 @@ function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
-  const format = new Intl.NumberFormat("en-US", {style: 'currency', currency: "USD", minimumFractionDigits: 2}).format;
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf)
 
