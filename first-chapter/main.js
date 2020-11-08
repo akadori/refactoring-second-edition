29db74362eabcfb515b17dbee3cfeb77903fbfa5
tamaritamari <tamari@minacare.co.jp>
2020-11-09 01:25:36 +0900

    amountFor

--- first-chapter/main.js
+++ first-chapter/main.js
@@ -1,3 +1,25 @@
+function amountFor(perf, play){    
+  let thisAmount =0;
+  switch(play.type) {
+    case "tragedy":
+      thisAmount = 40000;
+      if (perf.audience > 30) {
+        thisAmount += 1000 * (perf.audience - 30)
+      }
+      break;
+    case "comedy":
+      thisAmount = 30000;
+      if (perf.audience > 20) {
+        thisAmount += 10000 + 500 * (perf.audience - 20);
+      }
+      thisAmount += 300 * perf.audience
+      break;
+    default:
+      throw new Error(`unkown type: ${play.type}`)
+  }
+  return thisAmount
+}
+
 function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
@@ -5,25 +27,7 @@ function statement(invoice, plays) {
   const format = new Intl.NumberFormat("en-US", {style: 'currency', currency: "USD", minimumFractionDigits: 2}).format;
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
-    let thisAmount =0;
-
-    switch(play.type) {
-      case "tragedy":
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30)
-        }
-        break;
-      case "comedy":
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience
-        break;
-      default:
-        throw new Error(`unkown type: ${play.type}`)
-    }
+    let thisAmount = amountFor(perf, play)
     // ボリューム特典のポイントを加算
     volumeCredits += Math.max(perf.audience - 30, 0)
     // 喜劇の時は10人につき、さらにポイントを加算(なぜ5で割る: tamari append)
