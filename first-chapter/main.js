384d254b91b1732c0ef193c45065af0873e93ac1
tamaritamari <tamari@minacare.co.jp>
2020-11-09 21:52:36 +0900

    move total caluculate

--- first-chapter/main.js
+++ first-chapter/main.js
@@ -2,6 +2,8 @@ function statement(invoice, plays) {
   const statementData = {}
   statementData.customer = invoice.customer
   statementData.performances = invoice.performances.map(enrichPerformance)
+  statementData.totalAmount = totalAmount(statementData)
+  statementData.totalVolumeCredits = totalVolumeCredits(statementData)
   return renderPlaintext(statementData, plays)
 
   function enrichPerformance(aPerformance){
@@ -40,6 +42,20 @@ function statement(invoice, plays) {
     result += Math.max(aPerformance.audience - 30, 0)
     if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
     return result
+  }  
+  function totalAmount(data) {
+    let result = 0;
+    for (let perf of data.performances) {
+      result += perf.amount
+    }
+    return result
+  }
+  function totalVolumeCredits(data){
+    let result = 0;
+    for (let perf of data.performances) {
+      result += perf.volumeCredits
+    }
+    return result
   }
 }
 
@@ -49,29 +65,13 @@ function renderPlaintext(data) {
     // 注文の内訳を出力
     result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`
   }
-  result += `Amount owed is ${usd(totalAmount())}\n`;
-  result += `You earned ${totalVolumeCredits()} credits\n`;
+  result += `Amount owed is ${usd(data.totalAmount)}\n`;
+  result += `You earned ${data.totalVolumeCredits} credits\n`;
   return result;
 
-  function totalAmount() {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.amount
-    }
-    return result
-  }
-  function totalVolumeCredits(){
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.volumeCredits
-    }
-    return result
-  }
   function usd(aNumber) {
     return new Intl.NumberFormat("en-US", {style: 'currency', currency: "USD", minimumFractionDigits: 2}).format(aNumber/100);
   }
-
-
 }
 
 module.exports = statement
