ec90cb72102d0d416ea7f68688309511e5e5a63f
tamaritamari <tamari@minacare.co.jp>
2020-11-09 21:48:59 +0900

    remove volumeCreditsFor

--- first-chapter/main.js
+++ first-chapter/main.js
@@ -8,6 +8,7 @@ function statement(invoice, plays) {
     const result = Object.assign({}, aPerformance)
     result.play = playFor(result)
     result.amount = amountFor(result)
+    result.volumeCredits = volumeCreditsFor(result)
     return result
   }
   function playFor(aPerformance){
@@ -34,9 +35,15 @@ function statement(invoice, plays) {
     }
     return result
   }
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0)
+    if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
+    return result
+  }
 }
 
-function renderPlaintext(data, plays) {
+function renderPlaintext(data) {
   let result = `Statement for ${data.customer}\n`;
   for (let perf of data.performances) {
     // 注文の内訳を出力
@@ -56,18 +63,12 @@ function renderPlaintext(data, plays) {
   function totalVolumeCredits(){
     let result = 0;
     for (let perf of data.performances) {
-      result += volumeCreditsFor(perf)
+      result += perf.volumeCredits
     }
     return result
   }
   function usd(aNumber) {
     return new Intl.NumberFormat("en-US", {style: 'currency', currency: "USD", minimumFractionDigits: 2}).format(aNumber/100);
-  }
-  function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0)
-    if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
-    return result
   }
 
 
